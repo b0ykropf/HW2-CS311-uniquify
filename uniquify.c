ab192d4c9efbe9c89f714ba4763e82d4c9248ea3
Byron Kropf <b0ykropf@yahoo.com>
2012-02-03 09:41:52 -0800

    Now spawns argv[1]+1 children with the +1 being the suppressor and creates and closes all pipes as needed

--- uniquify.c
+++ uniquify.c
@@ -2,7 +2,7 @@
 *    Author: Byron Kropf
 *    Contact: kropfb@onid.orst.edu
 *    Created: January 25 2012
-*    Last Modified: January 25 2012
+*    Last Modified: February 1 2012
 *
 *    Explaination: Main file for the second assignment of CS311 which will
 *    read a text file and output all of the unique words in the file, in 
@@ -25,7 +25,7 @@
 
 struct kid_stats{
     int pid;
-    int ppipe[2];
+    int ipipe[2];
     FILE* file_out;
     struct kid_stats* next;
 };
@@ -35,48 +35,79 @@ typedef struct kid_stats kid;
 int main(int argc,char *argv[]){
 
     if( argc != 2){ //Check to see if enough arguments were given
-        err_exit("ERROR: You did not give 2 arguments.");
+        err_exit("ERROR: You did not give 1 arguments.");
     }
 
     int kid_count = atoi(argv[1]); //stores first argument as number of children to create
     pid_t kid_id;
-    
-    char *in_path;
-    char buffer[8];
+    pid_t kid_list[kid_count];// An array to store the pids of the kids. Not necessary.;
 
     int** pipe_farm = NULL;
-    pipe_farm = (int**) malloc( sizeof(int*) * kid_count); //mallocs memory for pointers
+    pipe_farm = (int**) malloc( sizeof(int*) * kid_count * 2); // mem for pipe pointers
 
     FILE** tmp_files = NULL;
-    tmp_files = (FILE**) malloc( sizeof(FILE*) * kid_count);
+    tmp_files = (FILE**) malloc( sizeof(FILE*) * kid_count); // mem for tmp pointers
 
     char* test = "This is a test.";
 
-    for( int i = 0; i < kid_count; i++ ){
+    size_t buffer = 8;
+
+    for( int i = 0; i < (2 * kid_count); i++ ){ //makes 2 pipes per child
         pipe_farm[i] = malloc( sizeof(int) * 2 );//malloc memory for pipes
+             
+  
         if( pipe(pipe_farm[i]) == -1 ) { //create pipes in pipe_farm
             err_exit("ERROR: Problem creating pipe");
         }
-        tmp_files[i] = malloc( sizeof(FILE*) );//malloc memory for temp files
-        tmp_files[i] = tmpfile(); //create temp files
-
+        
+    //Note: pipes 0-n are for parse to sort, and n+1-2n are for sort to suppress
     }
     
-    for( int i =0; i < kid_count; i++ ) {
+    for ( int i = 0; i < kid_count; i++){ // Create a temp file for each sorting child
+        tmp_files[i] = malloc( sizeof(FILE*) );
+        tmp_files[i] = tmpfile();
+    }
+ 
+    for( int i = 0; i <= kid_count; i++ ) {//spawns argv[1]+1 kids, +1 is suppressor
+       
         switch( ( kid_id = fork() ) ) { //Spawn Children (Pray that they do not zombify)
 
             case 0:
-                
-                if( close(pipe_farm[i][1]) != 0 ) {
-                    err_exit("ERROR:Problem plugging write end of pipe to child ",i);
-                }
-                
-                if( i != 0 ){
-                    for( int k = 0; i < k; k++ ) { //For each child close uneeded pipes
-                        close(*pipe_farm[k]);
+
+                if(i == kid_count){ //Special instructions for suppressor
+                    for( int j = 0; j < kid_count; j++){ //plug uneeded pipes
+                        close(*pipe_farm[j]);
                     }
-                }
+
+                    for( j = kid_count; j < (2 * kid_count); j++){ //plug write ends
+                        if( close(pipe_farm[j][1]) != 0) {
+                            err_exit("ERROR:Problem plugging write end pipe to suppres");
+                        }
+                    }
+                }//End special instructions for suppressor
+
+                if(i != kid_count){
+                    if( close(pipe_farm[i][1]) != 0 ) {
+                        err_exit("ERROR:Problem plugging write end of pipe to child ",i);
+                    }
+
+                    if( close(pipe_farm[kid_count + i][0]) != 0) { //plug read to suppres
+                        err_exit("ERROR:Problem plugging read end of pipe to suppressor");
+                    }
+
+                    for( int k = 0;k < kid_count;k++ ) { //children plug uneeded in pipes
+                        if(k != i){
+                            close(*pipe_farm[k]);
+                        }
+                    } 
            
+                   for( k = kid_count;k < (2 * kid_count); k++) { //also close uneeded out
+                       if(k != (kid_count + i){
+                           close(*pipe_farm[k]);
+                       }
+                   }
+                }
+
                 dup2(fileno(tmp_files[i]), 1);
                 dup2(pipe_farm[i][0], 0);
                 snprintf(argv[1], 1, "%d", i);
@@ -85,20 +116,30 @@ int main(int argc,char *argv[]){
 
             default:
                 
-                for(int i = 0; i < kid_count; i++) {
+                for(int i = 0; i < (2 * kid_count); i++) { //Parent never reads from pipe
                     if( close(pipe_farm[i][0]) ) {
                         err_exit("ERROR: Problem closeing read end of pipes for parent");
                     }
                 }
-                for(i = 0; i < kid_count; i++) {
+
+                for(i = 0; i < kid_count; i++) { //write to sort pipes and plug them
                     
                     write(pipe_farm[i][1], test, buffer); //write test to each pipe
                     close(pipe_farm[i][1]); //Plug the pipes
 
                 }
 
-                wait(NULL); //Wait for kids (DO NOT LET THEM ZOMBIFY!)
-                for(i = 0; i < kid_count; i++) {
+                for(i = kid_count; i < (2 * kid_count); i++) {
+                    
+                    close(pipe_farm[i][1]); //plug the rest of the pipes
+                }
+
+                
+                for(i = 0, i < kid_count; i++){ //Wait for all kids
+                    wait(NULL); //Wait for kids (DO NOT LET THEM ZOMBIFY!)
+                }
+
+                for(i = 0; i < kid_count; i++) { // TEMP REMOVE
                     fseek(tmp_files[i], 0, SEEK_SET);
                     fgets(buffer, 8, tmp_files[i]);
                     printf("%s\n", buffer);
